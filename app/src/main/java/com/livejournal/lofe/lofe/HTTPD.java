4af26246ebc031da15611786bcc988a70456d0ed
Shumel Vladimir <shumel.vladimir@gmail.com>
2020-04-17 23:24:57 +0300

    Закинул на телефон файлы веб-страницы, пробую загружать их. Получается частично

--- app/src/main/java/com/livejournal/lofe/lofe/HTTPD.java
+++ app/src/main/java/com/livejournal/lofe/lofe/HTTPD.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static android.os.Environment.getExternalStorageDirectory;
 import static com.livejournal.lofe.lofe.MyUtil.getCurTimeMS;
 import static com.livejournal.lofe.lofe.MyUtil.log;
 
@@ -41,42 +42,92 @@ class HTTPD {
             super(8080);
         }
 
+//        @Override
+//        public Response serve(IHTTPSession session) { // Рабочий первоначальный вариант
+//            String msg =
+//                "<html>" +
+//                    "<head>" +
+//                    "</head>" +
+//                    "<body>" +
+//                        //"<h1>Lofe</h1>\n" +
+//                        "<ul>";
+//                    //"<div id=\"chart_div\" style=\"width: 900px; height: 500px\"></div>" +
+//
+//            DB db = new DB(MyApplication.getContext());
+//            db.open();
+//            Cursor c = db.getAllData(getCurTimeMS());
+//            if (c.moveToFirst()) {
+//                do {
+//                    msg += "<li>" + c.getString(1) + "\n";
+//                } while (c.moveToNext());
+//            }
+//            c.close();
+//            db.close();
+//            //log("Запрос к серверу");
+//                    msg +=
+//                    "</body>" +
+//                "</html>\n";
+////            String msg = "<html><body><h1>Lofe</h1>\n";
+//            //Map<String, String> parms = session.getParms();
+//            Map<String, List<String>> parms = session.getParameters();
+////            if (parms.get("username") == null) {
+////                msg += "<form action='?' method='get'>\n";
+////                msg += "<p>Your name: <input type='text' name='username'></p>\n";
+////                msg += "</form>\n";
+////            } else {
+////                msg += "<p>Hello, " + parms.get("username") + "!</p>";
+////            }
+//            return newFixedLengthResponse(msg);
+//        }
+
         @Override
         public Response serve(IHTTPSession session) {
-            String msg =
-                "<html>" +
-                    "<head>" +
-                    "</head>" +
-                    "<body>" +
-                        //"<h1>Lofe</h1>\n" +
-                        "<ul>";
-                    //"<div id=\"chart_div\" style=\"width: 900px; height: 500px\"></div>" +
 
-            DB db = new DB(MyApplication.getContext());
-            db.open();
-            Cursor c = db.getAllData(getCurTimeMS());
-            if (c.moveToFirst()) {
-                do {
-                    msg += "<li>" + c.getString(1) + "\n";
-                } while (c.moveToNext());
+            String filePath = getExternalStorageDirectory().getAbsolutePath() + "/Lofe/www";
+
+//            Method method = session.getMethod();
+            String uri = session.getUri();
+//            Map<String, String> files = new HashMap<>();
+//            SharedPreferences prefs = OpenRAP.getContext().getSharedPreferences(MainActivity.mypreference, MODE_PRIVATE);
+//            OpenRAP app = (OpenRAP) OpenRAP.getContext();
+//            Storage storage = new Storage(OpenRAP.getContext());
+//            String currentpath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/www/";
+//            String temp = Environment.getExternalStorageDirectory().getAbsolutePath() + "/www/temp/";
+//            String ecarpath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/www/ecars_files/";
+//            String xcontent = Environment.getExternalStorageDirectory().getAbsolutePath() + "/www/xcontent/";
+            //String Endpoint = session.getUri();
+
+            log(uri);
+
+            if (uri.equals("/"))
+                filePath += "/index.html";
+            else
+                filePath += uri;
+
+            String answer = "";
+            try {
+                FileReader file = new FileReader(filePath); // Open file from SD Card
+                BufferedReader reader = new BufferedReader(file);
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    answer += line;
+                }
+                reader.close();
+            } catch (IOException ioe) {
+                Log.w("Httpd", ioe.toString());
             }
-            c.close();
-            db.close();
-            //log("Запрос к серверу");
-                    msg +=
-                    "</body>" +
-                "</html>\n";
-//            String msg = "<html><body><h1>Lofe</h1>\n";
-            //Map<String, String> parms = session.getParms();
-            Map<String, List<String>> parms = session.getParameters();
-//            if (parms.get("username") == null) {
-//                msg += "<form action='?' method='get'>\n";
-//                msg += "<p>Your name: <input type='text' name='username'></p>\n";
-//                msg += "</form>\n";
-//            } else {
-//                msg += "<p>Hello, " + parms.get("username") + "!</p>";
-//            }
-            return newFixedLengthResponse(msg);
+            return newFixedLengthResponse(answer);
+
+//            String msg = "<html><head></head><body><ul>";
+//            DB db = new DB(MyApplication.getContext());
+//            db.open();
+//            Cursor c = db.getAllData(getCurTimeMS());
+//            if (c.moveToFirst())
+//                do msg += "<li>" + c.getString(1) + "\n"; while (c.moveToNext());
+//            c.close();
+//            db.close();
+//            msg += "</body></html>\n";
+//            return newFixedLengthResponse(msg);
         }
     }
 }
